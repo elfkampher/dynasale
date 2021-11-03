700b3d2b5cc430f41d3a095d1a54feef884997e2
Juan Gutierrez P <elfkampher@gmail.com>
2021-11-03 01:26:56 -0600

    se crea la vista, forma y controlador del modulo de asignación de roles y permisos

--- app/Http/Livewire/Asignar.php
+++ app/Http/Livewire/Asignar.php
@@ -3,11 +3,104 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Livewire\WithPagination;
+use DB;
 
 class Asignar extends Component
 {
+    use WithPagination;
+
+    public $role, $componentName, $permisosSelected = [], $old_permissions = [];
+    private $pagination = 10;
+
+    public function paginationView()
+    {
+        return 'vendor.livewire.bootstrap';
+    }
+
+    public function mount()
+    {
+        $this->role = 'Elegir';
+        $this->componentName = 'Asignar Permisos';
+    }
+
     public function render()
     {
-        return view('livewire.asignar.asignar');
+        $permisos = Permission::select('name', 'id', DB::raw("0 as checked"))
+        ->orderBy('name', 'asc')
+        ->paginate($this->pagination);
+
+        if($this->role != 'Elegir')
+        {
+            $list = Permission::join('role_has_permissions as rp', 'rp.permission_id', 'permissions.id')
+            ->where('role_id', $this->role)->pluck('permissions.id')->toArray();
+
+            $this->old_permissions = $list;
+        }
+
+        if($this->role != 'Elegir')
+        {
+            foreach($permisos as $permiso)
+            {
+                $role = Role::find($this->role);
+                $tienePermiso = $role->hasPermissionsTo($permiso->name);
+                if($tienePermiso){
+                    $permiso->checked = 1;
+                }
+            }
+        }
+
+        return view('livewire.asignar.asignar' [
+            'roles' => Role::orderBy('name', 'asc')->get(), 
+            'permisos' => $permisos
+        ])->extends('layouts.theme.app')->section('content');
     }
+
+    public $listeners = ['revokeall' => 'RemoveAll'];
+
+    public function RemoveAll()
+    {
+        if($this->role == 'Elegir')
+        {
+            $this->emit('sync-error', 'Selecciona un rol válido');
+            return;
+        }
+
+        $role = Role::find($this->role);
+        $role->syncPermissions([0]);
+        $this->emit('removeall', "Se revocaron todos los permisos al $role->name");
+    }
+
+    public function SyncAll()
+    {
+        if($this->role == 'Elegir')
+        {
+            $this->emit('sync-error', 'Selecciona un rol válido');
+            return;
+        }
+
+        $role = Role::find($this->role);
+        $permisos = Permission::pluck('id')->toArray();
+        $role->syncPermissions($permisos);
+        $this->emit('syncall', "Se sincronizaron todos los permisos al $role->name");
+    }
+
+    public function syncPermiso($state, $permisoName)
+    {
+        if($this->role == 'Elegir')
+        {
+            $roleName = Role::find($this->role);
+            if($state)
+            {
+                $roleName->givePermissionTo($permisoName);
+                $this->emit('permi', 'Permiso asignado correctamente');
+            }else{
+                $roleName->revokePermissionTo($permisoName);
+                $this->emit('permi', 'Permiso eliminado correctamente');
+            }
+        }
+    }
+
 }
